8b4093e3c958dc688cf69a98eda54669a757e8b1
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-07-12 22:26:12 -0700

    paradime fix

--- soc_site/users/views.py
+++ soc_site/users/views.py
@@ -131,13 +131,13 @@ def view_by_tag(request, tag_slug):
     if medium == 'questions':
         tag = Tag.objects.filter(slug=tag_slug).first()
         object_list = Question.actives.filter(tags__in=[tag])
-        return render(request, 'feed/questions_by_tag.html', {'questions':object_list, 'c_tag':tag})
+        return render(request, 'feed/questions_by_tag.html', {'questions':object_list, 'c_tag':tag_slug})
 
     posts = Post.published.all()
     tag = Tag.objects.filter(slug=tag_slug).first()
     #tag = get_object_or_404(Tag, slug=tag_slug)
     object_list = posts.filter(tags__in=[tag])
-    return render(request, 'feed/posts_by_tag.html', {'posts':object_list, 'c_tag':tag})
+    return render(request, 'feed/posts_by_tag.html', {'posts':object_list, 'c_tag':tag_slug})
 
 
 
