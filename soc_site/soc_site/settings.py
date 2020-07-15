877295f702e355b11872492df8cf76f0366a41d2
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-07-14 23:29:06 -0700

    changed widget

--- soc_site/soc_site/settings.py
+++ soc_site/soc_site/settings.py
@@ -42,10 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.forms',
     'crispy_forms',
     'taggit',
     'markdownx',
 ]
+
+FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
